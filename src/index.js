ce70d65d95769832572429c1850b13a59ba8521b
Tomasz Szynkiewicz <tomasz.long@gmail.com>
2017-10-02 08:29:15 +0200

    Added redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {
+  createStore,
+  applyMiddleware
+} from 'redux';
+import { Provider } from 'react-redux';
+import createSagaMiddleware from 'redux-saga';
+
+import rootReducer from './reducers';
+import rootSaga from './sagas';
+
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const sagaMiddleware = createSagaMiddleware();
+
+function configureStore(initialState) {
+  const store = createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware));
+
+  store.sagaTask = sagaMiddleware.run(rootSaga);
+  return store;
+}
+
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
